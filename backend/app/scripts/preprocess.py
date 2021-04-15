70a15229308bafa8d713c490dfb69c13f60151c6
James Briggs <jfbriggs106@gmail.com>
2021-04-15 14:47:26 -0700

    Add CSV merging function, create aggregated CSV files

--- backend/app/scripts/preprocess.py
+++ backend/app/scripts/preprocess.py
@@ -1,4 +1,26 @@
+import os
+import pandas as pd
 
 
 def hello_world():
     return "Hello world!"
+
+
+# If separate folders of individual CSV files need to be aggregated
+def _merge_csv(source: str):
+    data_folders = ["skater_stats", "season_standings", "norris_voting"]
+    seasons = [filename.rstrip('.csv')[-8:] for filename in os.listdir(f"{source}/skater_stats")]
+
+    dataframes = {}
+
+    for season in seasons:
+        for folder in data_folders:
+            df = pd.read_csv(f"{source}/{folder}/{folder}_{season}.csv")
+            df["season"] = season
+            if dataframes.get(folder) is None:
+                dataframes[folder] = df
+            else:
+                dataframes[folder] = pd.concat([dataframes[folder], df]).reset_index(drop=True)
+
+    for df_name in dataframes:
+        dataframes[df_name].to_csv(os.path.join(source, f"{df_name}.csv"))
